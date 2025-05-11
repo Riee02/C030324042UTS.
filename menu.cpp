bbaa9451e166f76442389f4a1ab264ffa51830f5
Rifqi Fathoni <itsmeriee02@gmail.com>
2025-05-11 13:54:16 +0800

    Menambahkan menu Insertion Sort dan Ascending Descending

--- menu.cpp
+++ menu.cpp
@@ -2,8 +2,8 @@
 #include <conio.h>
 using namespace std;
 
-int aray[5];
-
+int aray[32];
+int n=5;
 void ingfo(){
   system("cls");
   cout<<"Indonesia Jaya,Poliban";
@@ -25,10 +25,10 @@ cout<<"hallo saya menu "<<pesan;
 getch();
 }
 
-void dataArray(int aray[]){
+void dataArray(int aray[],int n){
   system("cls");
-  cout << "Input 5 angka" << endl;
-  for(int i = 0; i < 5; i++) {
+  cout << "Input " << n << " angka" << endl;
+  for(int i = 0; i < n; i++) {
     cout << "Angka ke  " << (i + 1) << ": ";
     cin >> aray[i];
   }
@@ -38,8 +38,41 @@ getch();
 void OutputArr(){
   system("cls");
   cout << "Berikut Array yang telah di input" << endl;
-  for(int i = 0 ; i < 5; i++){
+  for(int i = 0 ; i < n; i++){
     cout << aray[i] << " ";
+  }
+  getch();
+}
+
+void InsertionSort(int aray[], int n, bool ascending) {
+  for (int i = 1; i < n; i++) {
+    int temp = aray[i];
+    int j = i - 1;
+    while (j >= 0 && ((ascending && aray[j] > temp) || (!ascending && aray[j] < temp))) {
+      aray[j + 1] = aray[j];
+      j--;
+    }
+    aray[j + 1] = temp;
+  }
+}
+
+void SortMenu() {
+  system("cls");
+  char pilihSort;
+  cout << "Pilih metode pengurutan:\n";
+  cout << "1. Ascending (Naik)\n";
+  cout << "2. Descending (Turun)\n";
+  cout << "Masukkan pilihan: ";
+  pilihSort = getch();
+
+  if (pilihSort == '1') {
+    InsertionSort(aray, n, true);
+    cout << "\nData berhasil diurutkan secara ASCENDING.";
+  } else if (pilihSort == '2') {
+    InsertionSort(aray, n, false);
+    cout << "\nData berhasil diurutkan secara DESCENDING.";
+  } else {
+    cout << "\nPilihan tidak valid!";
   }
   getch();
 }
@@ -53,16 +86,15 @@ do
   switch (pl)
   {
    case '1':
-    dataArray(aray);
+    dataArray(aray,n);
     break;
    case '2':
     OutputArr();
     /* code */ 
     break;  
    case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
+    SortMenu();
+    break;
    case '4':
     ingfo();
     /* code */
